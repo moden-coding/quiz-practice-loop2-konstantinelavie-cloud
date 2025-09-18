10011d9fdc3fc173714bd97724713e7f6047575d
Nadav Konstantine <nadavk2002@gmail.com>
2025-09-18 16:00:29 -0400

    done

--- src/App.java
+++ src/App.java
@@ -10,5 +10,20 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int highest = 0;
+        int lowest = 100;
+        while (true) {
+            System.out.println("Give a score: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                break;
+            } else if (number > highest) {
+                highest = number;
+            } 
+            if (number < lowest) {
+                lowest = number;
+            }
+            
+        } System.out.println("The highest score was " + highest + " and the lowest score was " + lowest);
     }
 }
